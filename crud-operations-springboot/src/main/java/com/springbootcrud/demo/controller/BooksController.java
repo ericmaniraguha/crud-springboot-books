de521eaaaff5a273681e1f85911e94e3f5d7911c
Eric Maniraguha <44385819+ericmaniraguha@users.noreply.github.com>
2023-06-28 11:59:13 +0200

    error handling and exceptions added
    In the above code, I've added exception handling, such as when a book is not found or when there is an internal server error. The appropriate HTTP response codes are returned along with the response entity.

--- crud-operations-springboot/src/main/java/com/springbootcrud/demo/controller/BooksController.java
+++ crud-operations-springboot/src/main/java/com/springbootcrud/demo/controller/BooksController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,68 +17,69 @@ import org.springframework.web.bind.annotation.RestController;
 import com.springbootcrud.demo.model.Books;
 import com.springbootcrud.demo.service.BooksService;
 
-//mark class as controller
 @RestController
 public class BooksController {
-   
-	//autowire the BooksService class  
-	@Autowired  
-	BooksService booksService;  
-	//creating a get mapping that retrieves all the books detail from the database   
-	@GetMapping("/book")  
-	private List<Books> getAllBooks()   
-	{  
-	return booksService.getAllBooks();  
-	}  
-	//creating a get mapping that retrieves the detail of a specific book  
-	@GetMapping("/book/{bookid}")  
-	private Books getBooks(@PathVariable("bookid") int bookid)   
-	{  
-	return booksService.getBooksById(bookid);  
-	}  
-	//creating a delete mapping that deletes a specified book  
-	@DeleteMapping("/book/{bookid}")  
-	private void deleteBook(@PathVariable("bookid") int bookid)   
-	{  
-	booksService.delete( bookid);  
-	}  
-	//creating post mapping that post the book detail in the database  
-//	@PostMapping("/books")  
-//	private int saveBook(@RequestBody Books books)   
-//	{  
-//	booksService.saveOrUpdate(books);  
-//	return books.getBookid();  
-//	}  
-	//create books, in json format using array format
-//	@PostMapping("/books")
-//	private List<Integer> saveBooks(@RequestBody List<Books> booksList) {
-//	    List<Integer> savedBookIds = new ArrayList<>();
-//	    for (Books books : booksList) {
-//	        booksService.saveOrUpdate(books);
-//	        savedBookIds.add(books.getBookid());
-//	    }
-//	    return savedBookIds;
-//	}
-//	
-	
-	// Modify the saveBooks method in BooksController class
-	@PostMapping("/books")
-	private List<Integer> saveBooks(@RequestBody List<Books> booksList) {
-	    List<Integer> savedBookIds = new ArrayList<>();
-//	    int bookid = 1; // Initial bookid value
-	    for (Books books : booksList) {
-//	        books.setBookid(bookid);
-	        booksService.saveOrUpdate(books);
-	        savedBookIds.add(books.getBookid());
-//	        bookid += 10; // Increment bookid by 10 for the next record
-	    }
-	    return savedBookIds;
-	}
-	//creating put mapping that updates the book detail   
-	@PutMapping("/books")  
-	private Books update(@RequestBody Books books)   
-	{  
-	booksService.saveOrUpdate(books);  
-	return books;  
-	}  
+
+    @Autowired
+    BooksService booksService;
+
+    @GetMapping("/books")
+    public ResponseEntity<List<Books>> getAllBooks() {
+        List<Books> books = booksService.getAllBooks();
+        return new ResponseEntity<>(books, HttpStatus.OK);
+    }
+
+    @GetMapping("/books/{bookid}")
+    public ResponseEntity<Books> getBookById(@PathVariable("bookid") int bookid) {
+        try {
+            Books book = booksService.getBooksById(bookid);
+            return new ResponseEntity<>(book, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/books")
+    public ResponseEntity<Integer> saveBook(@RequestBody Books book) {
+        try {
+            booksService.saveOrUpdate(book);
+            return new ResponseEntity<>(book.getBookid(), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/books/batch")
+    public ResponseEntity<List<Integer>> saveBooks(@RequestBody List<Books> booksList) {
+        List<Integer> savedBookIds = new ArrayList<>();
+        try {
+            for (Books book : booksList) {
+                booksService.saveOrUpdate(book);
+                savedBookIds.add(book.getBookid());
+            }
+            return new ResponseEntity<>(savedBookIds, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/books/{bookid}")
+    public ResponseEntity<Books> updateBook(@PathVariable("bookid") int bookid, @RequestBody Books book) {
+        try {
+            booksService.update(book, bookid);
+            return new ResponseEntity<>(book, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/books/{bookid}")
+    public ResponseEntity<Void> deleteBook(@PathVariable("bookid") int bookid) {
+        try {
+            booksService.delete(bookid);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
